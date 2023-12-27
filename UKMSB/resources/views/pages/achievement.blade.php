dcfafce0ca6496a2ba4e5a59370e37e4c37cc6b2
Yahyaa16 <yahyaashabulyamin@gmail.com>
2023-12-28 04:59:11 +0700

    update

--- UKMSB/resources/views/pages/achievement.blade.php
+++ UKMSB/resources/views/pages/achievement.blade.php
@@ -108,22 +108,21 @@
 
                                                     <div class="testimonial-block text-center">
                                                         <blockquote class="mb-5">
-                                                            <p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec
-                                                                vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
-                                                                velit. Aliquam vulputate velit imperdiet dolor tempor
-                                                                tristique. Pellentesque habitant morbi tristique senectus et
-                                                                netus et malesuada fames ac turpis egestas. Integer
-                                                                convallis volutpat dui quis scelerisque.&rdquo;</p>
+                                                            <p>&ldquo;Bergabung dengan UKM Sepak Bola Universitas Airlangga adalah
+                                                                keputusan terbaik dalam hidup saya selama menempuh pendidikan di sini.
+                                                                Saya merasa seperti menjadi bagian dari keluarga besar yang memiliki
+                                                                semangat dan dedikasi yang tinggi terhadap olahraga ini. Selain
+                                                                meningkatkan kemampuan teknis sepak bola saya, saya juga belajar banyak
+                                                                tentang kerja sama tim, kepemimpinan, dan mengelola tekanan dalam
+                                                                pertandingan. Terima kasih UKM Sepak Bola UA!.&rdquo;</p>
                                                         </blockquote>
 
                                                         <div class="author-info">
                                                             <div class="author-pic">
-                                                                <img src="images/person-1.png" alt="Maria Jones"
-                                                                    class="img-fluid">
+                                                                <img src="images/joko.jpg" alt="Maria Jones" class="img-fluid">
                                                             </div>
-                                                            <h3 class="font-weight-bold">Maria Jones</h3>
-                                                            <span class="position d-block mb-3">CEO, Co-Founder, XYZ
-                                                                Inc.</span>
+                                                            <h3 class="font-weight-bold">Joko Jones</h3>
+                                                            <span class="position d-block mb-3">Ilmu Komunikasi '19</span>
                                                         </div>
                                                     </div>
 
@@ -138,22 +137,21 @@
 
                                                     <div class="testimonial-block text-center">
                                                         <blockquote class="mb-5">
-                                                            <p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec
-                                                                vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
-                                                                velit. Aliquam vulputate velit imperdiet dolor tempor
-                                                                tristique. Pellentesque habitant morbi tristique senectus et
-                                                                netus et malesuada fames ac turpis egestas. Integer
-                                                                convallis volutpat dui quis scelerisque.&rdquo;</p>
+                                                            <p>&ldquo;Bergabung dengan UKM Sepak Bola Universitas Airlangga adalah
+                                                                fondasi penting dalam membentuk karakter dan keterampilan hidup saya.
+                                                                Melalui latihan yang intensif dan kompetisi di lapangan, saya belajar
+                                                                menghadapi tantangan dengan penuh semangat dan kesabaran. Keterampilan
+                                                                kepemimpinan dan kerja sama tim yang saya dapatkan di UKM ini juga
+                                                                membantu saya dalam karier profesional saya saat ini. Saya selalu
+                                                                bersyukur menjadi bagian dari komunitas ini.&rdquo;</p>
                                                         </blockquote>
 
                                                         <div class="author-info">
                                                             <div class="author-pic">
-                                                                <img src="images/person-1.png" alt="Maria Jones"
-                                                                    class="img-fluid">
+                                                                <img src="images/budi.jpg" alt="Maria Jones" class="img-fluid">
                                                             </div>
-                                                            <h3 class="font-weight-bold">Maria Jones</h3>
-                                                            <span class="position d-block mb-3">CEO, Co-Founder, XYZ
-                                                                Inc.</span>
+                                                            <h3 class="font-weight-bold">Michael Budi</h3>
+                                                            <span class="position d-block mb-3">Teknik Industri '20</span>
                                                         </div>
                                                     </div>
 
@@ -168,22 +166,21 @@
 
                                                     <div class="testimonial-block text-center">
                                                         <blockquote class="mb-5">
-                                                            <p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec
-                                                                vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
-                                                                velit. Aliquam vulputate velit imperdiet dolor tempor
-                                                                tristique. Pellentesque habitant morbi tristique senectus et
-                                                                netus et malesuada fames ac turpis egestas. Integer
-                                                                convallis volutpat dui quis scelerisque.&rdquo;</p>
+                                                            <p>&ldquo;UKM Sepak Bola Universitas Airlangga memberikan lebih dari sekadar
+                                                                latihan sepak bola. Saya memiliki kesempatan untuk mengasah keterampilan
+                                                                kepemimpinan dan organisasi melalui peran saya sebagai Ketua UKM. Hal
+                                                                ini membuka jalan bagi saya untuk memimpin tim dengan visi dan strategi
+                                                                yang jelas. Pengalaman ini tidak hanya memberikan wawasan tambahan dalam
+                                                                dunia olahraga, tetapi juga membentuk saya sebagai pemimpin yang tangguh
+                                                                di luar lapangan hijau.&rdquo;</p>
                                                         </blockquote>
 
                                                         <div class="author-info">
                                                             <div class="author-pic">
-                                                                <img src="images/person-1.png" alt="Maria Jones"
-                                                                    class="img-fluid">
+                                                                <img src="images/siti.jpg" alt="Maria Jones" class="img-fluid">
                                                             </div>
-                                                            <h3 class="font-weight-bold">Maria Jones</h3>
-                                                            <span class="position d-block mb-3">CEO, Co-Founder, XYZ
-                                                                Inc.</span>
+                                                            <h3 class="font-weight-bold">Siti Gou</h3>
+                                                            <span class="position d-block mb-3">Teknik Informatika '22</span>
                                                         </div>
                                                     </div>
 
@@ -191,7 +188,9 @@
                                             </div>
                                         </div>
                                         <!-- END item -->
+
                                     </div>
+
                                 </div>
                             </div>
                         </div>
